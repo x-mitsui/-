e353e60490ca3d139aeabe341e3b16dcdaaeac6c
dzq <x_mitsui@163.com>
2022-02-22 13:26:00 +0800

    修改app.use参数问题，应该为单参数

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const Koa = require("koa");
-const app = new Koa();
-const views = require("koa-views");
-const json = require("koa-json");
-const onerror = require("koa-onerror");
-const bodyparser = require("koa-bodyparser");
-const logger = require("koa-logger");
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
-const crawler = require("./routes/crawler");
+const crawler = require('./routes/crawler')
 // error handler
-onerror(app);
+onerror(app)
 
 // middlewares
 app.use(
   bodyparser({
-    enableTypes: ["json", "form", "text"],
+    enableTypes: ['json', 'form', 'text'],
   })
-);
-app.use(json());
-app.use(logger());
-app.use(require("koa-static")(__dirname + "/public"));
+)
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(
-  views(__dirname + "/views", {
-    extension: "pug",
+  views(__dirname + '/views', {
+    extension: 'pug',
   })
-);
+)
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
 
 // routes
-app.use(crawler.routes(), crawler.allowedMethods());
+
+app.use(crawler.routes()).use(crawler.allowedMethods())
 
 // error-handling
-app.on("error", (err, ctx) => {
-  console.error("server error", err, ctx);
-});
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+})
 
-module.exports = app;
+module.exports = app
