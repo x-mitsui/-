f631632186ef6402629ab04420e76ab9a1c6efdf
dzq <x_mitsui@163.com>
2022-02-25 12:01:08 +0800

    添加相应爬虫的访问路由

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,7 +1,15 @@
-const router = require("koa-router")();
+const router = require('koa-router')()
 
-const crawlerController = require("../controllers/Crawler");
-router.prefix("/crawler");
-router.get("/craw_slider_data", crawlerController.crawlSliderData);
+const crawlerController = require('../controllers/Crawler')
+router.prefix('/crawler')
+router.get('/crawl_slider_data', crawlerController.crawlSliderData)
+router.get('/crawl_agency_info', crawlerController.crawlAgencyInfo)
+router.get('/crawl_recom_course', crawlerController.crawlRecomCourse)
+router.get('/crawl_collection', crawlerController.crawlCollection)
+router.get('/crawl_teacher', crawlerController.crawlTeacher)
+router.get('/crawl_student', crawlerController.crawlStudent)
+router.get('/crawl_course_tab', crawlerController.crawlCourseTab)
+router.get('/crawl_course', crawlerController.crawlCourse)
+router.get('/crawl_about', crawlerController.crawlAbout)
 
-module.exports = router;
+module.exports = router
