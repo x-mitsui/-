5a6df50ae26c97dc1f999b1528de1d947a0b2d45
dzq <x_mitsui@163.com>
2022-02-25 12:00:04 +0800

    探究爬虫接口的测试

--- libs/crawler.js
+++ libs/crawler.js
@@ -1,22 +1,25 @@
-const pt = require("puppeteer");
+const pt = require('puppeteer')
 
 module.exports = async (options) => {
-  const browser = await pt.launch();
-  const url = options.url;
-  const pg = await browser.newPage();
+  /* Turn off headless mode - sometimes it's useful to see what the browser is displaying. Instead of launching in headless mode, launch a full version of the browser using headless: false:
+   */
+  // const browser = await pt.launch({ headless: false })
+  const browser = await pt.launch()
+  const url = options.url
+  const pg = await browser.newPage()
 
   await pg.goto(url, {
-    timeout: 30 * 1000,
-    waitUntil: "networkidle2", // 500ms只能没有发起连接，表示完成
-  });
+    timeout: 60 * 1000,
+    waitUntil: 'networkidle2', // 500ms只能没有发起连接，表示完成
+  })
 
-  const result = await pg.evaluate(options.callback);
+  const result = await pg.evaluate(options.callback) //获取浏览器那边执行options.callback后的result
 
-  await browser.close();
+  await browser.close()
 
-  process.send(result);
+  process.send(result) //send mes to main process
 
   setTimeout(() => {
-    process.exit(0);
-  }, 1000);
-};
+    process.exit(0)
+  }, 1000)
+}
--- libs/utils.js
+++ libs/utils.js
@@ -1,53 +1,54 @@
-const cp = require("child_process");
-const path = require("path");
+const cp = require('child_process')
+const path = require('path')
 
-const { nanoid } = require("nanoid");
-const Qiniu = require("qiniu");
+const { nanoid } = require('nanoid')
+const Qiniu = require('qiniu')
+const qiniuConfig = require('../config/config')
 
 module.exports = {
   startProcess(options) {
-    const script = path.resolve(__dirname, options.path);
-    const child = cp.fork(script, []);
-    let invoked = false;
+    const script = path.resolve(__dirname, '../crawlers/', options.file)
+    const child = cp.fork(script, [])
+    let invoked = false
 
-    child.on("message", (data) => {
-      options.message(data);
-    });
+    child.on('message', (data) => {
+      options.message(data)
+    })
 
-    child.on("exit", (code) => {
+    child.on('exit', (code) => {
       if (invoked) {
-        return;
+        return
       }
-      invoked = true;
-      options.exit(code);
-    });
+      invoked = true
+      options.exit(code)
+    })
 
-    child.on("error", (err) => {
+    child.on('error', (err) => {
       if (invoked) {
-        return;
+        return
       }
-      invoked = true;
-      options.error(err);
-    });
+      invoked = true
+      options.error(err)
+    })
   },
   qiniuUploads(options) {
-    const mac = new Qiniu.auth.digest.Mac(options.ak, options.sk);
-    const conf = new Qiniu.conf.Config();
-    const client = new Qiniu.rs.BucketManager(mac, conf);
-    const key = nanoid() + options.ext; //合成文件名
+    const mac = new Qiniu.auth.digest.Mac(qiniuConfig.qiniu.keys.ak, qiniuConfig.qiniu.keys.sk)
+    const conf = new Qiniu.conf.Config()
+    const client = new Qiniu.rs.BucketManager(mac, conf)
+    const key = nanoid() + options.ext //合成文件名
 
     return new Promise((resolve, reject) => {
       client.fetch(options.url, options.bucket, key, (error, ret, info) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
           if (info.statusCode === 200) {
-            resolve({ key });
+            resolve({ key })
           } else {
-            reject(info);
+            reject(info)
           }
         }
-      });
-    });
+      })
+    })
   },
-};
+}
