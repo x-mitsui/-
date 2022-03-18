ee1a8d3fdce9a713d4eee54458a95ccaba36a052
dzq <x_mitsui@163.com>
2022-03-18 15:10:01 +0800

    ...

--- app.js
+++ app.js
@@ -1,46 +1,72 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-
-const crawler = require('./routes/crawler')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const koaRedis = require("koa-redis");
+const { sessionInfo, cookieInfo, redisInfo, corsOrigin } = require("./config/config");
+const crawlerRouter = require("./routes/crawler");
+const indexRouter = require("./routes/index");
+const adminRouter = require("./routes/admin");
+const cors = require("koa2-cors");
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
 app.use(
-  bodyparser({
-    enableTypes: ['json', 'form', 'text'],
+  cors({
+    origin: function (ctx) {
+      return corsOrigin;
+    },
+    // 允许携带cookie验证信息
+    credentials: true,
   })
-)
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+);
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
 
 app.use(
-  views(__dirname + '/views', {
-    extension: 'pug',
+  views(__dirname + "/views", {
+    extension: "ejs",
   })
-)
+);
+
+app.keys = sessionInfo.keys; // 加密cookie的key
+app.use(
+  session({
+    key: sessionInfo.name, // cookie name
+    prefix: sessionInfo.prefix, //redis key 的前缀
+    cookie: cookieInfo, // cookie信息
+    store: koaRedis(redisInfo),
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
+app.use(crawlerRouter.routes()).use(crawlerRouter.allowedMethods());
 
-app.use(crawler.routes()).use(crawler.allowedMethods())
+app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
+app.use(adminRouter.routes()).use(adminRouter.allowedMethods());
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-})
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
 
-module.exports = app
+module.exports = app;
--- db/sync.js
+++ db/sync.js
@@ -1,17 +1,21 @@
-const seq = require('./connection/mysql_connect')
+const seq = require("./connection/mysql_connect");
 
-require('./models')
+require("./models");
 
 seq
   .authenticate()
   .then(() => {
-    console.log('connect mysql successfully')
+    console.log("connect mysql successfully");
   })
   .catch((err) => {
-    console.log('connect mysql falied:', err)
-  })
+    console.log("connect mysql falied:", err);
+  });
 // 建表 - 终端运行：node ./db/sync.js
-seq.sync({ force: true }).then(() => {
-  console.log('The table has been synchronized into database successfully!')
-  process.exit() //终止进程
-})
+seq
+  .sync({
+    // force: true
+  })
+  .then(() => {
+    console.log("The table has been synchronized into database successfully!");
+    process.exit(); //终止进程
+  });
