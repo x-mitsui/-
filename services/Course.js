201f171848a311e7248610761f3d3cbe13ab35d2
dzq <x_mitsui@163.com>
2022-03-18 15:09:14 +0800

    Course添加获取课程信息服务

--- services/Course.js
+++ services/Course.js
@@ -1,18 +1,25 @@
-const CourseModel = require('../db/models/course')
+const CourseModel = require("../db/models/course");
 class CourseService {
   async addCourse(data) {
-    const cid = data.cid
+    const cid = data.cid;
     const result = await CourseModel.findOne(data, {
       where: { cid },
-    })
+    });
     if (result) {
       return await CourseModel.update(data, {
         where: { cid },
-      })
+      });
     } else {
-      return await CourseModel.create(data)
+      return await CourseModel.create(data);
     }
+  }
+  async getCourses() {
+    return await CourseModel.findAll({
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "posterUrl", "description"],
+      },
+    });
   }
 }
 
-module.exports = new CourseService()
+module.exports = new CourseService();
