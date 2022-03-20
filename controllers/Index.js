e9d9ee4dfcd78046e15fd096ae3590aed11a51fb
dzq <x_mitsui@163.com>
2022-03-20 20:01:50 +0800

    getCourseData利用Promise.all并行获取数据

--- controllers/Index.js
+++ controllers/Index.js
@@ -1,33 +1,38 @@
-const { redisGet, redisSet } = require("../libs/redisClient");
-const { returnInfo } = require("../libs/utils");
-const { getCourses } = require("../services/Course");
-const { API } = require("../config/error_config");
+const { redisGet, redisSet } = require('../libs/redisClient')
+const { returnInfo } = require('../libs/utils')
+const { getCourses } = require('../services/Course')
+const { getCoursesTab } = require('../services/CourseTab')
+const { API } = require('../config/error_config')
 class Index {
   async index(ctx, next) {
-    const sess = ctx.session;
+    const sess = ctx.session
 
     if (!sess.uid) {
       // 往sess中存信息
-      sess.uid = 1;
-      sess.username = "jsjiajia";
-      sess.nickname = "js++";
-      sess.gender = "male";
+      sess.uid = 1
+      sess.username = 'jsjiajia'
+      sess.nickname = 'js++'
+      sess.gender = 'male'
     }
     // await redisSet("a", 1);
     // await redisSet("json", { a: 1, b: 2 }); //dui
     // let res = await redisGet("txclass01.sessz-4YSCYWU-opsPxf8FRaSkDywo5cUk39");
     // console.log(res);
     ctx.body = {
-      session: sess,
-    };
+      session: sess
+    }
     // ctx.render('index')
   }
 
-  async getCourseData(ctx, next) {
-    const data = await getCourses();
-
-    ctx.body = data ? returnInfo(API.GET_DATA_SUCCESS, data) : returnInfo(API.GET_DATA_FAILED);
+  async getCoursesData(ctx, next) {
+    try {
+      const data = await Promise.all([getCourses(), getCoursesTab()])
+      ctx.body = returnInfo(API.GET_DATA_SUCCESS, { courseData: data[0], fieldData: data[1] })
+    } catch (error) {
+      console.log(error)
+      ctx.body = returnInfo(API.GET_DATA_FAILED)
+    }
   }
 }
 
-module.exports = new Index();
+module.exports = new Index()
