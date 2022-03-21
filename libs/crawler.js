712efd0da592b2cd4bcf5e7f72c25426290c5bf6
dzq <x_mitsui@163.com>
2022-03-21 21:09:41 +0800

    记录一些爬虫接口，应对前端路由分页情况

--- libs/crawler.js
+++ libs/crawler.js
@@ -12,7 +12,16 @@ module.exports = async (options) => {
     timeout: 60 * 1000,
     waitUntil: 'networkidle2' // 500ms只能没有发起连接，表示完成
   })
-
+  /** 前端路由多页面情况
+   * await pg.waitForSelector('.check-btn');
+   * await pg.click('.check-btn')
+   * await pg.waitFor(2000);
+   * const res = await pg.evaluate(options.callback)
+   * await pg.waitFor(2000);
+   * for(var i=0;i<res.length; i++){
+   *    await result.push(res[i])
+   * }
+   */
   const result = await pg.evaluate(options.callback) //获取浏览器那边执行options.callback后的result
 
   await browser.close()
