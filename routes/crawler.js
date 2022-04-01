a9abf3d88ce78a6c7c1a1887d2587a6bd6a51dfe
dzq <x_mitsui@163.com>
2022-04-01 21:13:49 +0800

    crawler路由调整，中间件直接写在router中

--- routes/crawler.js
+++ routes/crawler.js
@@ -4,20 +4,20 @@ const crawlerController = require('../controllers/Crawler')
 const loginCheck = require('../middlewares/loginCheck')
 // 也考虑过使用动态路由+switch case+param中间件写法，但似乎没有这种直观
 router.prefix('/crawler')
-router.get('/crawl_slider_data', crawlerController.crawlSliderData)
-router.get('/crawl_agency_info', crawlerController.crawlAgencyInfo)
-router.get('/crawl_recom_course', crawlerController.crawlRecomCourse)
-router.get('/crawl_collection', crawlerController.crawlCollection)
-router.get('/crawl_teacher', crawlerController.crawlTeacher)
-router.get('/crawl_student', crawlerController.crawlStudent)
-router.get('/crawl_course_tab', crawlerController.crawlCourseTab)
-router.get('/crawl_course', crawlerController.crawlCourse)
-router.get('/crawl_about', crawlerController.crawlAbout)
+router.get('/crawl_slider_data', loginCheck, crawlerController.crawlSliderData)
+router.get('/crawl_agency_info', loginCheck, crawlerController.crawlAgencyInfo)
+router.get('/crawl_recom_course', loginCheck, crawlerController.crawlRecomCourse)
+router.get('/crawl_collection', loginCheck, crawlerController.crawlCollection)
+router.get('/crawl_teacher', loginCheck, crawlerController.crawlTeacher)
+router.get('/crawl_student', loginCheck, crawlerController.crawlStudent)
+router.get('/crawl_course_tab', loginCheck, crawlerController.crawlCourseTab)
+router.get('/crawl_course', loginCheck, crawlerController.crawlCourse)
+router.get('/crawl_about', loginCheck, crawlerController.crawlAbout)
 
 router.post('/crawl_action', crawlerController.crawlAction.bind(crawlerController))
 // router.post('/crawl_about', crawlerController.crawlAbout)
 // 批量给各个路由添加前置中间件
-router.stack.forEach((route) => {
-  route.stack.unshift(loginCheck)
-})
+// router.stack.forEach((route) => {
+//   route.stack.unshift(loginCheck)
+// })
 module.exports = router
